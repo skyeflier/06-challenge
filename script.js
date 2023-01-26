2329eded955c51b8c610ca298e3e8bb466133111
skyeflier <119133572+skyeflier@users.noreply.github.com>
2023-01-25 19:51:28 -0600

    adding local storage function

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 var cityName = document.querySelector('.city-name');
-var temp = document.querySelector('.temperature');
-var tempCurrent = document.querySelector('#temperature-current')
-var humidity = document.querySelector('.humidity');
-var humidityCurrent = document.querySelector('#humidity-current')
-var wind = document.querySelector('.wind');
-var windCurrent = document.querySelector('#wind-current');
-var cityInput = document.querySelector('.city-input'); //getElementbyClassName returns an array
+var dateCurrent = document.querySelector('.date-current');
 
-var cityNameHistoryArray = [];
+var iconCurrentEL = document.createElement('iconCurrent');
+var tempCurrent = document.querySelector('#temperature-current');
+var humidityCurrent = document.querySelector('#humidity-current');
+var windCurrent = document.querySelector('#wind-current');
+
+// var iconEL = document.createElementNS('icon');
+var temp = document.querySelector('.temperature');
+var humidity = document.querySelector('.humidity');
+var wind = document.querySelector('.wind');
+
+var cityInput = document.querySelector('.city-input'); //getElementbyClassName returns an array
+var historyContainerElement = document.querySelector('#search-history-container');
+
+var cityNameHistoryArray = JSON.parse(localStorage.getItem("cities")) || [] // Setting a default - Check to see if there is something in local storage, if there is nothing in local storage, then set this variable to an empty array
 
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
@@ -19,6 +26,7 @@ const searchButton = document.querySelector('#search-button') // Needs to be in 
 searchButton.addEventListener('click', function () { // When the user types in a city and clicks 'search', cityData function is called to call the weather data they are requesting. 
     var cityValue = cityInput.value; // The value property sets or returns the value of the value attribute of a text field. In this case, the user input of a city name. 
     cityData(cityValue); // Calling the city Data function WHY CAN WE NOT USE CITYINPUT? WHY DO WE NEED TO CREATE A NEW VALUE
+    saveSearch();
 });
 
 function cityData(cityInfo) { // cityData is a function that is calling the openweather API and pulling in City information. 
@@ -35,76 +43,81 @@ function cityData(cityInfo) { // cityData is a function that is calling the open
 
 function currentWeather(lat, lon) {
     var geoApi = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial"
+    iconCurrentEL.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
     fetch(geoApi)
         .then((response) => response.json())
         .then((data) => {
-            var info = data.list[29];
+            // var info = data.list[29];
+            var info = data.list[0];
             displayData(info);
         })
     // .then((data) => console.log(data.list));
 }
 
+http://openweathermap.org/img/wn/weather.icon.@2x.png
+
 function displayData(weatherInfo) {
+    var city = cityInput.value
+    let cityNameEl = document.querySelector("#city-name");
+    cityNameEl.innerHTML = city.toUpperCase();
+    // <img src="http://openweathermap.org/img/wn/" ${
+    //     weatherInfo.weather[0].icon
+    // }
+    // dateCurrent.innerHTML = weatherInfo.dt_txt
+    // iconCurrent = weatherInfo.
     tempCurrent.innerHTML = weatherInfo.main.temp + "°F"
+    console.log(weatherInfo);
     humidityCurrent.innerHTML = weatherInfo.main.humidity + "%"
     windCurrent.innerHTML = weatherInfo.wind.speed + " " + "mph"
-    localStorage.setItem("cityInput", weatherInfo);
-    console.log(weatherInfo);
+
+
+    // cityNameHistoryArray.push(city)
+    // localStorage.setItem("cities", JSON.stringify(cityNameHistoryArray));
+    // console.log(weatherInfo);
 }
+
+function onLoad() {
+    if (cityNameHistoryArray !== 0) { // if it's not an empty array in storage then loop through what we have and display
+        for (let i = 0; i < cityNameHistoryArray.length; i++) {
+            let liEl = document.createElement("li")
+            let historyButton = document.createElement("button")
+            historyButton.textContent = cityNameHistoryArray[i] //The text content of this button
+            liEl.appendChild(historyButton)
+            historyContainerElement.appendChild(liEl)
+        }
+    }
+}
+
+function saveSearch() { //when we load, we'll grab everything in storage 
+    var city = cityInput.value
+    cityNameHistoryArray.push(city)
+    localStorage.setItem("cities", JSON.stringify(cityNameHistoryArray));
+}
+
+// when looping thorugh that array of cities, attach an event listener to each buttons and pass in a value (the text content of those buttons)
+// call function city data and pass in that current search value 
+
+// loop through images http://openweathermap.org/img/wn/
+
 
 function searchHistory() {
-    cityNameHistoryArray = localStorage.getItem('cityName');
-    if (cityNameHistoryArray) {
-        var storedCities = parseInt(this.dataset.cityNameHistoryArray) // json parse cityNameHistoryArray
-        var savedCities = cityNameHistoryArray.push(this) // then .push new city name into array
-    } else {
-        cityNameHistoryArray = [cityName]; // this is if there is nothing in local storage. we cant push to the array if it does not exist. So we redefine it with the new city name inside.
-    }
-    // var cities = localStorage.getItem(cityNameHistoryArray);
-    // cityNameHistoryArray.value
-
-    // console.log(JSON.stringify(storedCities));
-    // console.log(JSON.stringify(savedCities));
 }
 
-// I can say that you can push the users input or search into the array and save it in the array. Then a forloop can be used to loop through it and create the buttons and list that shows on the screen
 
-JSON.stringify(cityNameHistoryArray) // json stringify cityNameHistoryArray
+// JSON.stringify(cityNameHistoryArray) // json stringify cityNameHistoryArray
 
-localStorage.setItem('cityName', cityNameHistoryArray);
+// localStorage.setItem('cityName', cityNameHistoryArray);
 
-if (cityNameHistoryArray) {
-    // Json parse cityNameHistoryArray
+// if (cityNameHistoryArray) {
+//     // Json parse cityNameHistoryArray
 
-    for (var i = 0; i < cityNameHistoryArray.length; i++) {
-        // create button element
-        // use .textcontent to add text from cityNameHistoryArray[i] to button
-        // append button to already existing element in the html
-    }
-}
-
+//     for (var i = 0; i < cityNameHistoryArray.length; i++) {
+//         // create button element
+//         // use .textcontent to add text from cityNameHistoryArray[i] to button
+//         // append button to already existing element in the html
+//     }
+// }
 
 // function weatherForecast() {
 
-// loop through images http://openweathermap.org/img/wn/
-// }
-
-// function searchHistory() {
-//     var recentSearch = []
-//     recentSearch.push
-//     var cityInput = data.name
-//     var cityName = localStorage.getItem("cityInput")
-//     document.querySelector('cityInput').value = cityName;
-// }
-
-// TO DO - CREATE A FUNCTION FOR THE FORECAST DAYS
-// TO DO - store data in the local
-
-
-// Need to get info from list item for each info (city name, wind, etc. )
-// to display on the page, use .text content on each element from lin 1-5 for current weather
-
-// Then for forecast, 0 first day 7 is second day 15 next day 23, 31, 39 (every 3 hours displays a time, so you need to move across the index of that array)
-// each index
-
-// LOOK INTO FUNCTIONS, DEFINING & CALLING THEM
+onLoad()
