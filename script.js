173d72ffc797aca0e1f8197fc8943583b33510b0
skyeflier <119133572+skyeflier@users.noreply.github.com>
2023-01-29 12:55:41 -0600

    Added variables for forecast days

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
 var cityName = document.querySelector('.city-name');
-var dateCurrent = document.querySelector('.date-current');
 
+//Current Weather Variables
+var dateCurrent = document.querySelector('.date-current');
 var currentWeatherIcon = document.querySelector('.current-weather-icon');
 var iconCurrentEL = document.createElement('iconCurrent');
 var tempCurrent = document.querySelector('#temperature-current');
 var humidityCurrent = document.querySelector('#humidity-current');
 var windCurrent = document.querySelector('#wind-current');
 
-// var iconEL = document.createElementNS('icon');
-var temp = document.querySelector('.temperature');
-var humidity = document.querySelector('.humidity');
-var wind = document.querySelector('.wind');
+//Five Day Forecast Variables
+var forecastWeatherIcon = document.querySelector('.weather-icon-forecast');
+var iconForecastEL = document.createElement('iconForecast');
+var tempForecast = document.querySelector('.temperature-forecast');
+var humidityForecast = document.querySelector('.humidity-forecast');
+var windForecast = document.querySelector('.wind-forecast');
 
-var cityInput = document.querySelector('.city-input'); //getElementbyClassName returns an array
+var dateDayOne = document.querySelector('#date-day-one');
+var dateDayTwo = document.querySelector('#date-day-two');
+var dateDayThree = document.querySelector('#date-day-three');
+var dateDayFour = document.querySelector('#date-day-four');
+var dateDayFive = document.querySelector('#date-day-five');
+
+var iconDayOne = document.querySelector('#icon-day-one');
+var iconDayTwo = document.querySelector('#icon-day-two');
+var iconDayThree = document.querySelector('#icon-day-three');
+var iconDayFour = document.querySelector('#icon-day-four');
+var iconDayFive = document.querySelector('#icon-day-five');
+
+var temperatureDayOne = document.querySelector('#temperature-day-one');
+var temperatureDayOne = document.querySelector('#temperature-day-two');
+var temperatureDayOne = document.querySelector('#temperature-day-three');
+var temperatureDayOne = document.querySelector('#temperature-day-four');
+var temperatureDayOne = document.querySelector('#temperature-day-five');
+
+var humidityDayOne = document.querySelector('#humidity-day-one');
+var humidityDayTwo = document.querySelector('#humidity-day-two');
+var humidityDayThree = document.querySelector('#humidity-day-three');
+var humidityDayFour = document.querySelector('#humidity-day-four');
+var humidityDayFive = document.querySelector('#humidity-day-five');
+
+var windDayOne = document.querySelector('#wind-day-one');
+var windDayTwo = document.querySelector('#wind-day-two');
+var windDayThree = document.querySelector('#wind-day-three');
+var windDayFour = document.querySelector('#wind-day-four');
+var windDayFive = document.querySelector('#wind-day-five');
+
+
+
+
+var cityInput = document.querySelector('.city-input');
 var historyContainerElement = document.querySelector('#search-history-container');
 
 var cityNameHistoryArray = JSON.parse(localStorage.getItem("cities")) || [] // Setting a default - Check to see if there is something in local storage, if there is nothing in local storage, then set this variable to an empty array
 
 var apiKey = "47a5adb4b320deb886afffad5e515b45";
-
-// User story: page loads, type in city name, click search, grab a city value, use that value to perform the API call, pull the current & forecasted weather from the API, and store search history
 
 const searchButton = document.querySelector('#search-button') // Needs to be in the global scope, because the button needs to be ready when the page is loaded
 searchButton.addEventListener('click', function () { // When the user types in a city and clicks 'search', cityData function is called to call the weather data they are requesting. 
@@ -32,7 +66,6 @@ searchButton.addEventListener('click', function () { // When the user types in a
 function cityData(cityInfo) { // cityData is a function that is calling the openweather API and pulling in City information. 
     console.log(cityInfo)
     var geoApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInfo + "&appid=" + apiKey //This is how to connect a variable to a string 
-    // console.log(geoApi);
     fetch(geoApi)
         .then((response) => response.json()) // This translates the original data to readable data in JSON format
         .then((data) => { // This now allows you to work with the data from openWeather
@@ -43,7 +76,6 @@ function cityData(cityInfo) { // cityData is a function that is calling the open
 
 function currentWeather(lat, lon) {
     var geoApi = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial"
-    // iconCurrentEL.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
     fetch(geoApi)
         .then((response) => response.json())
         .then((data) => {
@@ -51,32 +83,19 @@ function currentWeather(lat, lon) {
             var info = data.list[0];
             displayData(info);
         })
-    // .then((data) => console.log(data.list));
 }
-
-// http://openweathermap.org/img/wn/weather.icon.@2x.png
 
 function displayData(weatherInfo) {
     var city = cityInput.value
     let cityNameEl = document.querySelector("#city-name");
-    let imageEl = document.createElement('img')
-    imageEl.src = `https://openweathermap.org/img/w/${weatherInfo.weather[0].icon}@2x.png`
+    let imageEl = document.querySelector('#icon-current');
+    imageEl.src = `https://openweathermap.org/img/w/${weatherInfo.weather[0].icon}.png`
     currentWeatherIcon.appendChild(imageEl)
-
     cityNameEl.innerHTML = city.toUpperCase();
-    // <img src="http://openweathermap.org/img/wn/" ${
-    //     weatherInfo.weather[0].icon
-    // }
-    // dateCurrent.innerHTML = weatherInfo.dt_txt
-    // iconCurrent = weatherInfo.
     tempCurrent.innerHTML = weatherInfo.main.temp + "°F"
     console.log(weatherInfo);
     humidityCurrent.innerHTML = weatherInfo.main.humidity + "%"
     windCurrent.innerHTML = weatherInfo.wind.speed + " " + "mph"
-
-    // cityNameHistoryArray.push(city)
-    // localStorage.setItem("cities", JSON.stringify(cityNameHistoryArray));
-    // console.log(weatherInfo);
 }
 
 function onLoad() {
@@ -103,51 +122,10 @@ function onLoad() {
 
 function saveSearch(cityValue) { //when we load, we'll grab everything in storage 
     var city = cityInput.value
-    // var city = cityValue.trim()
     cityNameHistoryArray.push(city);
     localStorage.setItem("cities", JSON.stringify(cityNameHistoryArray));
 }
 
-// function searchHistory() {
-//     //     var storedHistory = localStorage.getItem('search-history');
-//     //     if (storedHistory) {
-//     //         searchHistory = JSON.parse(storedHistory);
-//     //     }
-//     //     renderSearchHistory();
-// }
 
-// when looping thorugh that array of cities, attach an event listener to each buttons and pass in a value (the text content of those buttons)
-// call function city data and pass in that current search value 
-
-// loop through images http://openweathermap.org/img/wn/
-
-// ---------------------
-
-// function renderForecast(dailyForecast) {
-
-//     var startDt = dayjs().add(1, 'day').startOf('day').unix();
-//     var endDt = dayjs().add(6, 'day').startOf('day').unix();
-
-//     function searchHistory() {
-//     }
-
-
-// JSON.stringify(cityNameHistoryArray) // json stringify cityNameHistoryArray
-
-// localStorage.setItem('cityName', cityNameHistoryArray);
-
-// if (cityNameHistoryArray) {
-//     // Json parse cityNameHistoryArray
-
-//     for (var i = 0; i < cityNameHistoryArray.length; i++) {
-//         // create button element
-//         // use .textcontent to add text from cityNameHistoryArray[i] to button
-//         // append button to already existing element in the html
-//     }
-// }
-
-// function weatherForecast() {
 
 onLoad()
-
-// -------
